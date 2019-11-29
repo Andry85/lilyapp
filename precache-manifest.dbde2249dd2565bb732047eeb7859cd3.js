d6403123a3f9a6be1582f47b065a7211262ebe9d
Andry85 <shapovala@ukr.net>
2019-11-29 13:40:25 +0200

    Updates

---
+++ precache-manifest.dbde2249dd2565bb732047eeb7859cd3.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0948379928c497a97375b6ee6bb3dcba",
+    "url": "/lilyapp/index.html"
+  },
+  {
+    "revision": "47c644b2bd2e5fd50100",
+    "url": "/lilyapp/static/css/main.9858aff2.chunk.css"
+  },
+  {
+    "revision": "60e824b5eb53054af262",
+    "url": "/lilyapp/static/js/2.717eb127.chunk.js"
+  },
+  {
+    "revision": "47c644b2bd2e5fd50100",
+    "url": "/lilyapp/static/js/main.211e3d68.chunk.js"
+  },
+  {
+    "revision": "035c282c7f0cec316286",
+    "url": "/lilyapp/static/js/runtime-main.8ba40ae5.js"
+  },
+  {
+    "revision": "eb880f59af92cd1eb64db72bea163f5d",
+    "url": "/lilyapp/static/media/gena.eb880f59.png"
+  },
+  {
+    "revision": "fff0fc73081eeba35741dd5aca43d75a",
+    "url": "/lilyapp/static/media/header_bg.fff0fc73.png"
+  },
+  {
+    "revision": "623002de4474db20f70094466bfef284",
+    "url": "/lilyapp/static/media/ifs_01.623002de.png"
+  },
+  {
+    "revision": "dd7fe938e55a35cc478cdc72b7f7e027",
+    "url": "/lilyapp/static/media/ifs_01.dd7fe938.jpg"
+  },
+  {
+    "revision": "2d00d6c8be44adf7485a6f6cd94d31ad",
+    "url": "/lilyapp/static/media/ifs_02.2d00d6c8.png"
+  },
+  {
+    "revision": "f03bbdf61700d51cc0d621657ad0d9cb",
+    "url": "/lilyapp/static/media/ifs_02.f03bbdf6.jpg"
+  },
+  {
+    "revision": "9aece1ff96b391fb73d6517d71de2187",
+    "url": "/lilyapp/static/media/ifs_03.9aece1ff.png"
+  },
+  {
+    "revision": "d70be953bd69d8491c80ff0d8e7758b4",
+    "url": "/lilyapp/static/media/maryna.d70be953.png"
+  },
+  {
+    "revision": "2172fe9724b1b5301ea14bc544e04939",
+    "url": "/lilyapp/static/media/mehinvest_01.2172fe97.png"
+  },
+  {
+    "revision": "229037b655143afd14647d9057f0b6f0",
+    "url": "/lilyapp/static/media/mehinvest_02.229037b6.png"
+  },
+  {
+    "revision": "58ff2e90b64482b39e8083bb97064f79",
+    "url": "/lilyapp/static/media/mehinvest_03.58ff2e90.png"
+  },
+  {
+    "revision": "c8470ab8cad92bf01e6fa71722697c67",
+    "url": "/lilyapp/static/media/mi_01.c8470ab8.jpg"
+  },
+  {
+    "revision": "3ad3da07d0bf8593bd748684c77ecc4e",
+    "url": "/lilyapp/static/media/mi_02.3ad3da07.jpg"
+  },
+  {
+    "revision": "3ea72980b0a006a2b8522802efdb6abd",
+    "url": "/lilyapp/static/media/oksana.3ea72980.png"
+  },
+  {
+    "revision": "da504eb8b0a8f3515f60f533dc3f80a7",
+    "url": "/lilyapp/static/media/olga.da504eb8.png"
+  },
+  {
+    "revision": "9750dbd802d8f01d9a5affba2991d493",
+    "url": "/lilyapp/static/media/vitukraina_01.9750dbd8.png"
+  },
+  {
+    "revision": "2630a690542b7427b1f5f092efae0e19",
+    "url": "/lilyapp/static/media/vitukraina_02.2630a690.png"
+  },
+  {
+    "revision": "95e69b3bb350891a0faad8723223aea6",
+    "url": "/lilyapp/static/media/vitukraina_03.95e69b3b.png"
+  },
+  {
+    "revision": "a484e7d4c14c398935f5560113f9c57a",
+    "url": "/lilyapp/static/media/vuk_01.a484e7d4.jpg"
+  },
+  {
+    "revision": "0bc28c8bb1c1ab84f62237756606bca8",
+    "url": "/lilyapp/static/media/vuk_02.0bc28c8b.jpg"
+  }
+]);
