755ff02b6c69431529e1eec9620762dbd1a98f25
Andrii Shapoval <andrii.shapoval@smile-ukraine.com>
2020-08-06 18:01:53 +0300

    added email

---
+++ server/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    USER: 'andryshapoval@gmail.com', 
+    PASS: '72297229n'
+}
--- src/PopupForm/PopupForm.js
+++ src/PopupForm/PopupForm.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import './PopupForm.css';
 import {addLetter} from '../actions/actions';
+import axios from 'axios';
 
 
 class PopupForm extends React.Component {
@@ -10,9 +11,9 @@ class PopupForm extends React.Component {
     this.myRefForm = React.createRef();
     
     this.state = {
-      value: 'Enter Your message',
       name: 'Enter Your name',
       email: 'Enter Your email',
+      message: 'Enter Your message'
     };
 
     this.handleTextArea = this.handleTextArea.bind(this);
@@ -32,44 +33,60 @@ class PopupForm extends React.Component {
     this.setState({email: event.target.value});
   }
   handleTextArea(event) {
-    this.setState({value: event.target.value});
+    this.setState({message: event.target.value});
     
   }
 
   handleSubmit(event) {
     event.preventDefault();
-    this.props.addMessage(this.state.value, this.state.name, this.state.email);
+    console.log(this.state);
     this.myRefForm.current.classList.remove("visible");
+
+    axios({
+      method: "POST", 
+      url:"http://localhost:3002/send", 
+      data:  this.state
+    }).then((response)=>{
+      if (response.data.status === 'success'){
+        alert("Message Sent."); 
+        this.resetForm()
+      }else if(response.data.status === 'fail'){
+        alert("Message failed to send.")
+      }
+    })
+
   }
 
   hidepopup() {
     this.myRefForm.current.classList.remove("visible");
   }
 
-  componentDidMount() {
-
-  }
+  
+  resetForm(){
+    
+    this.setState({name: '', email: '', message: ''})
+ }
 
   render() {
     return (
       <div id="PopupForm" className="PopupForm" ref={this.myRefForm}>
         <div className="PopupForm__form">
           <span onClick={this.hidepopup} className="PopupForm__close"></span>
-          <form className="PopupForm__formIneer" onSubmit={this.handleSubmit}>
+          <form className="PopupForm__formIneer" onSubmit={this.handleSubmit} method="POST">
             <div className="PopupForm__row">
               <div className="PopupForm__col">
                 <label className="PopupForm__label">Your name:</label>
-                <input className="PopupForm__field" type="text" onChange={this.handleName} placeholder={this.state.name} />
+                <input className="PopupForm__field" type="text" onChange={this.handleName} value={this.state.name} />
               </div>
               <div className="PopupForm__col">
                 <label className="PopupForm__label">Your email:</label>
-                <input className="PopupForm__field" type="email" onChange={this.handleEmail} placeholder={this.state.email} />
+                <input className="PopupForm__field" type="email" onChange={this.handleEmail} value={this.state.email} />
               </div>  
             </div>
             <div className="PopupForm__row">
               <div className="PopupForm__col--full">
                 <label className="PopupForm__label">Your message:</label>
-                <textarea className="PopupForm__field PopupForm__field--area" onChange={this.handleTextArea} placeholder={this.state.value}/>
+                <textarea className="PopupForm__field PopupForm__field--area" onChange={this.handleTextArea} value={this.state.message} />
               </div>
             </div>
             <button className="PopupForm__btn">SEND</button>
